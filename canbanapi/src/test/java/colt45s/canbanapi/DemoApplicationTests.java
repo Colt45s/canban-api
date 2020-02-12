3f71001b42c9397fe08e9cfef9e5068a0221735a
Colt45s <kzk0693@gmail.com>
2020-02-12 22:43:23 +0900

    feat: add initialized by Spring Initializr

---
+++ canbanapi/src/test/java/colt45s/canbanapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package colt45s.canbanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
